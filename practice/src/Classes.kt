3f44e609452b3c5b3876c434619e51c192e36a6c
Maulik Dave <mdave.5191@gmail.com>
2024-10-14 16:04:22 +0530

    initizer block

---
+++ practice/src/Classes.kt
@@ -0,0 +1,40 @@
+
+class Car (private var name: String,
+           private var model: String,
+           private var color: String,
+           private var doors: Int) {
+
+    // init block
+    // called at instance of this class gets created.
+
+    fun move() {
+        println("Car is moving")
+    }
+
+    fun stop() {
+        println("The car is stopped")
+    }
+
+    override fun toString(): String {
+        return """
+            Name: $name
+            Model: $model
+            Color: $color
+            Doors: $doors
+        """
+    }
+}
+
+class User (name: String, var lastName: String, var age: Int) {
+
+    // initializer block
+    var name: String
+    init {
+        if (name.isNotEmpty() && name.uppercase().startsWith("M")) {
+            this.name = name
+        } else {
+            this.name = "No valid name provided."
+        }
+    }
+
+}
---
+++ practice/src/Main.kt
@@ -0,0 +1,11 @@
+
+    fun main(args: Array<String>){
+        val car1 = Car("Ford", "Mustang", "Black", 2)
+        println(car1.toString())
+
+
+        var user = User("Maulik", "Dave", 33)
+        println(user.toString())
+
+    }
+
